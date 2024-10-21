0c7811f42a47b4ea38194e3059ba083b0320d075
Destiny Morrison <112641813+DestinyMorrison@users.noreply.github.com>
2024-10-21 14:45:26 -0400

    Add files via upload

---
+++ Running_Project/app/src/main/java/com/zybooks/running_project/AchievementsActivity.java
@@ -0,0 +1,16 @@
+package com.zybooks.running_project;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AchievementsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievements);
+
+        // Load achievements here
+    }
+}
+
